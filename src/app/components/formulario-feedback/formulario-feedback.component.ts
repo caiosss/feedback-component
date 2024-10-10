a31f9493d0513b24c5326b8c30508df7b317be8f
Caio <caiobarros1712@gmail.com>
2024-10-10 18:43:14 -0300

    Ajustes

--- src/app/components/formulario-feedback/formulario-feedback.component.ts
+++ src/app/components/formulario-feedback/formulario-feedback.component.ts
@@ -17,6 +17,7 @@ export class FormularioFeedbackComponent {
   ]
 
   enableComments: boolean = false
+  exibirMensagem: boolean = false
 
   feedback: Feedback = {
     respostas: [],
@@ -30,19 +31,17 @@ export class FormularioFeedbackComponent {
 
   valor(question: any, event: any) {
     if (question && event && event.target && event.target.value !== null) {
-      question.resposta = event.target.value;
+      question.resposta = event.target.value
 
-      console.log("Resposta atualizada para a pergunta:", question.question, "é", question.resposta);
+      console.log(question.resposta)
 
-      const existingIndex = this.feedback.respostas.findIndex(r => r.pergunta === question.question);
+      const existingIndex = this.feedback.respostas.findIndex(r => r.pergunta === question.question)
       
       if (existingIndex !== -1) {
-        this.feedback.respostas[existingIndex].resposta = question.resposta;
+        this.feedback.respostas[existingIndex].resposta = question.resposta
       } else {
         this.feedback.respostas.push(question.resposta)
       }
-
-      console.log("Respostas até agora:", this.feedback.respostas); 
   } else {
     console.log('Erro: question ou event está nulo.')
   }
@@ -60,12 +59,13 @@ export class FormularioFeedbackComponent {
   constructor(private feedbackService: FeedbackService) { }
 
   onSubmit(): void {
-    console.log('Respostas enviadas:', this.feedback.respostas);
-    console.log('Respostas enviadas:', this.feedback);
-
+    console.log('Respostas enviadas:', this.feedback.respostas)
+    console.log('Respostas enviadas:', this.feedback)
+    
     this.feedbackService.enviarFeedback(this.feedback).subscribe(
       response => {
         console.log("Feedback enviado ", response)
+        this.exibirMensagem = true
       },
       error => {
         console.error("Erro ao enviar o feedback ", error)
